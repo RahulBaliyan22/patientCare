61b1dcd2a142c9e4638ffe321a19a6ccd0925fc6
Rahul Baliyan <rahulbaliyan863@gmail.com>
2025-01-23 00:42:25 +0530

    problem

--- back/middleware.js
+++ back/middleware.js
@@ -111,6 +111,8 @@ const isVerified = async (req, res, next) => {
 };
 
 const isLoggedIn = (req, res, next) => {
+  console.log('Session ID:', req.sessionID); // Log session ID
+  console.log('Is Authenticated:', req.isAuthenticated())
   if (!req.isAuthenticated()) {
     return res.status(401).json({ message: "Please log in first" }); // 401 Unauthorized
   }
