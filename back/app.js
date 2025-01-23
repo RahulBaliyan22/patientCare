2ef4b3b99b10b0eca76ebab1b38ce1bfd2ff414c
RahulBaliyan22 <154693504+RahulBaliyan22@users.noreply.github.com>
2025-01-23 12:24:50 +0530

    Update app.js

--- back/app.js
+++ back/app.js
@@ -9,6 +9,7 @@ const dashboardRoutes = require("./route/dashboard");
 const passport = require("passport");
 const session = require("express-session");
 const Patient = require("./model/Patient");
+const LocalStrategy = require('passport-local')
 const contactRoutes = require("./route/contact");
 const recordRoutes = require("./route/record");
 const patientRoutes = require("./route/patient");
@@ -57,15 +58,28 @@ app.use(
   })
 );
 
-
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
+  cookie:{
+    httpOnly:true,
+    expires:Date.now()+7*24*60*60*1000,
+     secure: true, 
+    sameSite: "None,
+    maxAge: 7*24*60*60*1000
+  }
+}));
 
 // Initialize passport
 app.use(passport.initialize());
 app.use(passport.session()); // Ensure passport session is used after initializing
 
 // Passport strategy using 'passport-local-mongoose'
-passport.use(Patient.createStrategy()); // Using the strategy created by passport-local-mongoose
-
+ // Using the strategy created by passport-local-mongoose
+// passport.use(Patient.createStrategy());
+passport.use(new LocalStrategy(Patient.authenticate()))
 passport.serializeUser(Patient.serializeUser()); // Automatically handles serializing user
 passport.deserializeUser(Patient.deserializeUser()); // Automatically handles deserializing user
 
