a89a772572585e9baed152851a0609a3d317bd0e
Rahul Baliyan <rahulbaliyan863@gmail.com>
2025-03-28 14:14:20 +0530

    ho ja

--- back/config/s3.js
+++ back/config/s3.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const AWS = require("aws-sdk");
 
-// Create and export a single S3 instance
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
-});
+function initializeS3() {
+    return new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+    });
+}
 
-module.exports = s3;
+module.exports = initializeS3;
