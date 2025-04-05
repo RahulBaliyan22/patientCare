5be28d58a11f4d607e995ae925ba2b952cfa8b2f
Rahul Baliyan <rahulbaliyan863@gmail.com>
2025-04-06 00:40:43 +0530

    ReloadlistContext

--- Frontend/src/components/Admin/Components/ListPatient.jsx
+++ Frontend/src/components/Admin/Components/ListPatient.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useContext } from 'react';
 import axios from 'axios';
 import '../Components/Styles/PatientList.css';
 import { Link } from 'react-router-dom';
