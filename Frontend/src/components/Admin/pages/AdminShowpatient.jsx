378e9eb6358a6faa39f617e63156da371a0709c7
Rahul Baliyan <rahulbaliyan863@gmail.com>
2025-04-06 20:53:32 +0530

    done

--- Frontend/src/components/Admin/pages/AdminShowpatient.jsx
+++ Frontend/src/components/Admin/pages/AdminShowpatient.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import "./AdminShowPatient.css";
+import "../Components/Styles/AdminShowPatient.css";
 
 const AdminShowPatient = () => {
   const { id } = useParams();
