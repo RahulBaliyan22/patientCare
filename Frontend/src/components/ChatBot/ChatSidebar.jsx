b570c3edefff09141ab7722d68f20b0d5f7c0c84
Rahul Baliyan <rahulbaliyan863@gmail.com>
2025-04-09 06:50:19 +0530

    final chatbot

--- Frontend/src/components/ChatBot/ChatSidebar.jsx
+++ Frontend/src/components/ChatBot/ChatSidebar.jsx
@@ -93,8 +93,16 @@ const ChatSidebar = ({ onClose, messages, setMessages, socket, role }) => {
           placeholder="Type a message..."
           onKeyDown={(e) => e.key === "Enter" && handleSend()}
         />
-        <button className="chat-wrapper__input__button" onClick={handleSend}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+        <button className="chat-wrapper__input__button" onClick={handleSend} style={{width: "fit-content"}}>
+        <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#FFFFFF"
+              >
+                <path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z" />
+              </svg>
         </button>
       </div>
     </div>
