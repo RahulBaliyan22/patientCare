e164e344a109ca393553d9d07ca24d89278ab03e
Rahul Baliyan <rahulbaliyan863@gmail.com>
2025-04-09 18:55:27 +0530

    basic layout changes

--- back/utils/vitalRealTime.js
+++ back/utils/vitalRealTime.js
@@ -1,17 +1,56 @@
 const {authorizeRole} = require('../middleware')
 
-module.exports = (io)=>{
-  const vitalsNamespace = io.of("/vitals");
+ heartInfo= (io)=>{
+  const vitalsNamespace = io.of("/vital-heart-rate");
 
   
   vitalsNamespace.use(authorizeRole("patient"))
-  
+
   vitalsNamespace.on("connection",(socket)=>{
+    console.log("heart connected")
+    socket.on("start-machine",()=>{
 
-
+    })
 
     socket.on("disconnect", () => {
       console.log("🔴user disconnected");
     });
   })
 }
+
+spoInfo= (io)=>{
+  const vitalsNamespace = io.of("/vital-spo2");
+
+  
+  vitalsNamespace.use(authorizeRole("patient"))
+
+  vitalsNamespace.on("connection",(socket)=>{
+    console.log("spo2 connected")
+    socket.on("start-machine",()=>{
+
+    })
+
+    socket.on("disconnect", () => {
+      console.log("🔴user disconnected");
+    });
+  })
+}
+
+tempInfo= (io)=>{
+  const vitalsNamespace = io.of("/vital-body-temp");
+
+  
+  vitalsNamespace.use(authorizeRole("patient"))
+
+  vitalsNamespace.on("connection",(socket)=>{
+    console.log("body temp connected")
+    socket.on("start-machine",()=>{
+
+    })
+    socket.on("disconnect", () => {
+      console.log("🔴user disconnected");
+    });
+  })
+}
+
+module.exports = {heartInfo,tempInfo,spoInfo}
