20a2952d3422ee0e95d5ebd42e0b8765ecad2dcb
Rahul Baliyan <rahulbaliyan863@gmail.com>
2025-04-09 06:58:23 +0530

    final chatbot

--- Frontend/src/components/ChatBot/ChatSidebar.jsx
+++ Frontend/src/components/ChatBot/ChatSidebar.jsx
@@ -50,25 +50,24 @@ const ChatSidebar = ({ onClose, messages, setMessages, socket, role }) => {
       <div className="chat-wrapper__header">
         <h3>{role?.charAt(0).toUpperCase() + role?.slice(1)} Chat</h3>
         <button
-  style={{
-    width: "30px",
-    height: "30px",
-    borderRadius: "50%",
-    backgroundColor: "#f0f0f0",
-    border: "none",
-    cursor: "pointer",
-    fontWeight: "bold",
-    transition: "background-color 0.2s ease",
-    display: "flex",              
-    justifyContent: "center",     
-    alignItems: "center",         
-    padding: 0                    
-  }}
-  onClick={onClose}
->
-  x
-</button>
-
+          style={{
+            width: "30px",
+            height: "30px",
+            borderRadius: "50%",
+            backgroundColor: "#f0f0f0",
+            border: "none",
+            cursor: "pointer",
+            fontWeight: "bold",
+            transition: "background-color 0.2s ease",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 0,
+          }}
+          onClick={onClose}
+        >
+          x
+        </button>
       </div>
 
       <div className="chat-wrapper__messages">
@@ -93,18 +92,27 @@ const ChatSidebar = ({ onClose, messages, setMessages, socket, role }) => {
           placeholder="Type a message..."
           onKeyDown={(e) => e.key === "Enter" && handleSend()}
         />
-        <button className="chat-wrapper__input__button" onClick={handleSend} style={{width: "fit-content", display: "flex",              
-    justifyContent: "center",     
-    alignItems: "center", marginLeft:"20px"}}>
-        <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#FFFFFF"
-              >
-                <path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z" />
-              </svg>
+        <button
+          className="chat-wrapper__input__button"
+          onClick={handleSend}
+          style={{
+            width: "fit-content",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: "20px",
+          }}
+        >
+          <svg
+            style={{margin:"0"}}
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#FFFFFF"
+          >
+            <path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z" />
+          </svg>
         </button>
       </div>
     </div>
